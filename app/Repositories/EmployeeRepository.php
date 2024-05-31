f4b3dd8d25fa5ee23b27bd6db65bc59337110977
Ishfaq Alvi <ishfaq.alvi.33@gmail.com>
2024-05-31 22:34:11 +0500

    Update

--- app/Repositories/EmployeeRepository.php
+++ app/Repositories/EmployeeRepository.php
@@ -172,6 +172,12 @@ class EmployeeRepository implements EmployeeInterface
         if(isset($data['order_status']) && $task->task_type == 'App\Models\Order'){
             $this->order->orderUpdate(['status' => $data['order_status']], $task->task_id, 'employee');
         }
+        if($task->status == 'Accept' && $task->task_type == 'App\Models\MaintenenceRequest'){
+            $this->maintenence->update(['status' => 'Ready to go'], $task->task_id, 'employee');
+        }
+        if($task->status == 'Ongoing' && $task->task_type == 'App\Models\MaintenenceRequest'){
+            $this->maintenence->update(['status' => 'Out for Maintenance'], $task->task_id, 'employee');
+        }
         if($task->status == 'Completed' && $task->task_type == 'App\Models\MaintenenceRequest'){
             $this->maintenence->update(['status' => 'Done'], $task->task_id, 'employee');
         }
